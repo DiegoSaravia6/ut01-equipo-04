de2c9719febdae57c24ec7741c205e04aab32952
Durluc1324 <lucianorodriguezpaven@gmail.com>
2026-08-22 18:50:22 -0300

    Ejercicio 26 - Parte B completa

---
+++ src/main/java/ucu/edu/aed/ejercicio26/ExpresionStack.java
@@ -0,0 +1,48 @@
+package ucu.edu.aed.ejercicio26;
+import java.util.Stack;
+
+import ucu.edu.aed.tda.Pila;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ExpresionStack {
+
+    public boolean controlCorchetes(List<Character> listaDeEntrada){
+
+        
+
+        if (listaDeEntrada == null){
+            throw new IllegalArgumentException("La lista de entrada no puede ser nula");
+        }
+        Stack<Character> revision = new Stack<>(); 
+
+        for(Character caracter : listaDeEntrada){
+            if(caracter == '{' || caracter == '(' || caracter == '['){
+                revision.push(caracter);
+            }
+
+            if(caracter == '}' || caracter == ')' || caracter == ']'){
+                if(revision.isEmpty()){
+                    return false;
+                }
+
+                Character tope = revision.peek();
+
+                if( (caracter == '}' && tope =='{' ) || 
+                (caracter == ')' && tope == '(')  || 
+                (caracter == ']' && tope == '[')){
+
+                    revision.pop();
+
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return revision.isEmpty();
+
+    }
+
+}
